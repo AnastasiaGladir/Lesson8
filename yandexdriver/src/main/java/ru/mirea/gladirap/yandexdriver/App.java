d2cf683f7af274feca49e5f2a0b050f4399d642a
Anastasia <nasti.listic@mail.ru>
2023-06-07 01:40:25 +0300

    Initial commit

---
+++ yandexdriver/src/main/java/ru/mirea/gladirap/yandexdriver/App.java
@@ -0,0 +1,15 @@
+package ru.mirea.gladirap.yandexdriver;
+
+import android.app.Application;
+
+import com.yandex.mapkit.MapKitFactory;
+
+public class App extends Application {
+    private final String MAPKIT_API_KEY = "2c2c82fd-c4d5-4b31-a2f3-f7879784c262";
+    @Override
+    public void onCreate() {
+        super.onCreate();
+// Set the api key before calling initialize on MapKitFactory.
+        MapKitFactory.setApiKey(MAPKIT_API_KEY);
+    }
+}
